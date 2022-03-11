1f10635ae88833fd807fb9c527fb222c55341059
Liliana Gomez <gomez.liliana333@gmail.com>
2022-03-10 23:09:08 -0500

    Left and right movement with keyboard for the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
 
@@ -34,8 +35,17 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+                    # # Move the ship to the right.
+                    # self.ship.rect.x += 1
 
     
     def _update_screen(self):
